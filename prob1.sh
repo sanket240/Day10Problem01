ae7f38695eabef189677a9f4f41e9a6f5b3f97de
sanket2498 <sanketdulange24@gmail.com>
2020-11-04 20:43:30 +0530

    Added : Descending Sort

--- prob1.sh
+++ prob1.sh
@@ -18,3 +18,18 @@ for((i=1;i<5;i++))
 do
 	arr[$i]=${Dic[$i]}
 done
+for ((i = 0; i<5; i++))
+do
+	for((j = 0; j<5-i-1; j++))
+	do
+		if [[ ${arr[j]} -lt ${arr[$((j+1))]} ]]
+        	then
+            		# swap
+            		temp=${arr[j]}
+            		arr[$j]=${arr[$((j+1))]}
+            		arr[$((j+1))]=$temp
+        	fi
+    	done
+done
+echo "Array sorted in Descending order :"
+echo ${arr[*]}
